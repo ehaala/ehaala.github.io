5932a48c444f01b1d223f3b8a4daf3c50754ec65
ehaala <ejhaala@gmail.com>
2017-11-07 15:00:20 -0800

    added resumes. added hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
-$('#test').typeIt({
+$('#bio').typeIt({
      // strings: ["Hi, I'm Evan the web developer.", "I currently reside in Seattle.", "I like to code, play video games, and anything sports-related."],
-     speed: 40,
+     speed: 35,
      breakLines: true,
      autoStart: true
 })
-.tiType("Hi, I'm Evan. I develop webs....?")
+.tiType("Hi, I'm Evan. I.... develop webs?")
 .tiPause(1500)
-.tiDelete(21)
+.tiDelete(18)
 .tiPause(500)
-.tiType(' the web developer.')
+.tiType(' am a web developer.')
 .tiBreak()
 .tiPause(1000)
 .tiType("I currently reside in Seattle.")
 .tiBreak() .tiPause(750)
 .tiType("I like to code, play video games, and anything sports-related.")
 .tiBreak() .tiPause(500)
-.tiType("Check out some of my projec")
-.tiPause(900)
-.tiDelete(6)
-.tiType("DOPE projects below!")
+.tiType("Check out some of my projects below!")
+
+$(document).ready(function(){
+    $("#resume").click(function(){
+        $("#myModal").modal();
+    });
+});
+
+$(function() { 
+	$("#projlink").on('click', function() { 
+		var position = $("#projects").offset().top; 
+		$("HTML, BODY").animate({ scrollTop: position }, 1000); 
+	}); 
+});
+
+$(function() { 
+	$("#conlink").on('click', function() { 
+		var position = $("#contact").offset().top; 
+		$("HTML, BODY").animate({ scrollTop: position }, 1000); 
+	}); 
+});

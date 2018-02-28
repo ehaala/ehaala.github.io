cb5b1c7d59244430cd4b3faad736a3b18cbe27ec
ehaala <ejhaala@gmail.com>
2018-02-28 15:04:00 -0800

    cleaned up some bad code

--- js/script.js
+++ js/script.js
@@ -1,42 +1,27 @@
 $(document).ready(function(){
 
-  $('#bio').typeIt({
-    // strings: ["Hi, I'm Evan the web developer.", "I currently reside in Seattle.", "I like to code, play video games, and anything sports-related."],
-    speed: 35,
-    breakLines: true,
-    autoStart: true
-  })
-  .tiType("Hi, I'm Evan. I am a web developer.")
-  .tiBreak()
-  .tiPause(1000)
-  .tiType("I currently reside in Seattle.")
-  .tiBreak() .tiPause(750)
-  .tiType("I like to code, play video games, and anything sports-related.")
-  .tiBreak() .tiPause(500)
-  .tiType("Check out some of my projects below!");
-
   $("#resume").click(function(){
     $("#resumeModal").modal();
   });
 
-  function testAnim(x) {
-      $('.modal .modal-dialog').attr('class', 'modal-dialog  ' + x + '  animated');
-  };
+  // function testAnim(x) {
+  //     $('.modal .modal-dialog').attr('class', 'modal-dialog  ' + x + '  animated');
+  // };
 
-  $('#pokemonModal').on('show.bs.modal', function (e) {
-    var anim = 'fadeInUpBig'
-        testAnim(anim);
-  })
+  // $('#pokemonModal').on('show.bs.modal', function (e) {
+  //   var anim = 'fadeInUpBig'
+  //       testAnim(anim);
+  // })
 
-  $('#gymderModal').on('show.bs.modal', function (e) {
-    var anim = 'fadeInUpBig'
-        testAnim(anim);
-  })
+  // $('#gymderModal').on('show.bs.modal', function (e) {
+  //   var anim = 'fadeInUpBig'
+  //       testAnim(anim);
+  // })
 
-  $('#habitrackModal').on('show.bs.modal', function (e) {
-    var anim = 'fadeInUpBig'
-        testAnim(anim);
-  })
+  // $('#habitrackModal').on('show.bs.modal', function (e) {
+  //   var anim = 'fadeInUpBig'
+  //       testAnim(anim);
+  // })
 
   $(function() { 
     $("#projlink").on('click', function() { 
